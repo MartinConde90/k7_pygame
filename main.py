125738e5f24c50096fb3d80d01077844a9ca8cac
Revellyon <martincondegrande@hotmail.com>
2020-01-29 04:52:10 +0100

    Refactorizar clase game

--- main.py
+++ main.py
@@ -94,54 +94,69 @@ class Robot:
     def image(self):
         return self.frames[self.frame_act]
 
-screen = display.set_mode((800, 600)) #control de pantalla, parentesis devuelve la instancia de una ventana con el tamaño que demos, pygame.display...como arriba
-display.set_caption('Hola mundo!') #titulo, pygame.display...etc
-clock = time.Clock() #pygame.time.clock
-
-background_color = (150, 150, 222) #color de fondo pantalla
 
 
-robot = Robot(400, 300)
+class Game:
+    clock = time.Clock() #pygame.time.clock
 
-bombas = []
-for i in range(5):
-    bomb = Bomb(randint(0 ,750), randint(0, 550))
-    bombas.append(bomb)
+    def __init__(self):
+       self.screen = display.set_mode((800, 600)) #control de pantalla, parentesis devuelve la instancia de una ventana con el tamaño que demos, pygame.display...como arriba
+       display.set_caption('Hola mundo!') #titulo, pygame.display...etc, va sin self por ser de pygame
+       
+       self.background_color = (150, 150, 222) #color de fondo pantalla
+
+       self.robot = Robot(400, 300)
+
+       self.bombas = []
+       for i in range(5):
+          self.bomb = Bomb(randint(0 ,750), randint(0, 550))
+          self.bombas.append(self.bomb)
+
+    def gameOver(self):
+        quit()
+        sys.exit()
+
+    def handleEvents(self):
+        for ev in event.get(): #pygame.event.get()
+            if ev.type == QUIT: #si cierras la ventana
+                self.gameOver()
+
+            if ev.type == KEYDOWN: #si pulsamos una tecla
+                if ev.key == K_UP:
+                    self.robot.go_up()
+                if ev.key == K_DOWN:
+                    self.robot.go_down()
+                if ev.key == K_LEFT:
+                    self.robot.go_left()
+                if ev.key == K_RIGHT:
+                    self.robot.go_right()
+
+        keys_pressed = key.get_pressed() #teclas que se mantienen pulsadas
+        if keys_pressed[K_UP]:
+            self.robot.go_up()
+        if keys_pressed[K_DOWN]:
+            self.robot.go_down()
+        if keys_pressed[K_RIGHT]:
+            self.robot.go_right()
+        if keys_pressed[K_LEFT]:
+            self.robot.go_left()
 
 
-while True:
-    dt = clock.tick(FPS) #para controlar los frames
+    def mainloop(self):
+        while True:
+            dt = self.clock.tick(FPS) #para controlar los frames
 
-    for ev in event.get(): #pygame.event.get()
-        if ev.type == QUIT: #si cierras la ventana
-             quit()
-             sys.exit()
+            self.handleEvents()
+            
+                #...Procesar el resto de eventos
+            self.screen.fill(self.background_color) #sobre screen, le metemos el color
+            self.screen.blit(self.robot.image, self.robot.position) #pones esa imagen donde te digo, ya no hace falta poner robot.position() al poner arriba @property
+            for b in self.bombas:
+                b.update(dt)
+                self.screen.blit(b.image, b.position)
 
-        if ev.type == KEYDOWN: #si pulsamos una tecla
-            if ev.key == K_UP:
-                robot.go_up()
-            if ev.key == K_DOWN:
-                robot.go_down()
-            if ev.key == K_LEFT:
-                robot.go_left()
-            if ev.key == K_RIGHT:
-                robot.go_right()
+            display.flip() # esto coge todo lo que se ha hecho, y lo mete en la ventana
 
-    keys_pressed = key.get_pressed() #teclas que se mantienen pulsadas
-    if keys_pressed[K_UP]:
-        robot.go_up()
-    if keys_pressed[K_DOWN]:
-        robot.go_down()
-    if keys_pressed[K_RIGHT]:
-        robot.go_right()
-    if keys_pressed[K_LEFT]:
-        robot.go_left()
-    
-        #...Procesar el resto de eventos
-    screen.fill(background_color) #sobre screen, le metemos el color
-    screen.blit(robot.image, robot.position) #pones esa imagen donde te digo, ya no hace falta poner robot.position() al poner arriba @property
-    for b in bombas:
-        b.update(dt)
-        screen.blit(b.image, b.position)
-
-    display.flip() # esto coge todo lo que se ha hecho, y lo mete en la ventana
+if __name__ == '__main__':
+    game = Game()
+    game.mainloop()
