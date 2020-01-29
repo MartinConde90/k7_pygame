7f4f03257c3124dfe41a9ad79f2fc9e6e25ba03c
Revellyon <martincondegrande@hotmail.com>
2020-01-29 04:50:09 +0100

    Pre refactorizar bomba

--- main.py
+++ main.py
@@ -1,8 +1,46 @@
 from pygame import *
 from pygame.locals import *
 import sys #libreria de sistema, para que cuando cerremos el juego, cierre la ventana
+from random import randint
 
 init() #inicia pygame, si hubiesemos puesto import pygame, esto seria, pygame.init()
+FPS = 60
+class Bomb:
+    pictures = ['bomb_01.png', 'bomb_02.png','bomb_03.png','bomb_04.png']
+
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+        self.frames = [] #con esto tenemos ya las imagenes, no las rutas a las imagenes
+        for pict in self.pictures:
+            frame = image.load('resources/{}'.format(pict)).convert_alpha()
+            self.frames.append(frame)
+       #ésto es como lo de arriba ---> self.frames = [image.load('resources/{}'.format(pict)).convert_alpha() for pict in self.pictures]
+
+        self.frame_act = 0
+        self.num_frames = len(self.frames)
+
+        self.current_time = 0 #tiempo actual
+        self.animation_time = FPS//444 #fraccion de las FPS
+
+    def update(self, dt):
+        self.current_time += dt
+        if self.current_time >= self.animation_time: #si el tiempo transcurrido desde que llamas a la funcion, osea que se inicia, es mayor que el animation_time, animas las bombas
+            self.current_time = 0
+            self.frame_act +=1
+            if self.frame_act == self.num_frames:
+                self.frame_act = 0
+            #ésto es lo mismo que de arriba ---> self.frame_act = (self.frame_act + 1) % self.num_frames
+
+    @property #ahora es un propiedad, no un método, como image
+    def position(self):
+        return self.x, self.y #esto lo devuelve en forma de tupla (x, y)
+
+    @property
+    def image(self):
+        return self.frames[self.frame_act]
+
 
 
 class Robot:
@@ -62,10 +100,17 @@ clock = time.Clock() #pygame.time.clock
 
 background_color = (150, 150, 222) #color de fondo pantalla
 
+
 robot = Robot(400, 300)
 
+bombas = []
+for i in range(5):
+    bomb = Bomb(randint(0 ,750), randint(0, 550))
+    bombas.append(bomb)
+
+
 while True:
-    clock.tick(60) #para controlar los frames
+    dt = clock.tick(FPS) #para controlar los frames
 
     for ev in event.get(): #pygame.event.get()
         if ev.type == QUIT: #si cierras la ventana
@@ -92,11 +137,11 @@ while True:
     if keys_pressed[K_LEFT]:
         robot.go_left()
     
-        
-
-
         #...Procesar el resto de eventos
     screen.fill(background_color) #sobre screen, le metemos el color
     screen.blit(robot.image, robot.position) #pones esa imagen donde te digo, ya no hace falta poner robot.position() al poner arriba @property
+    for b in bombas:
+        b.update(dt)
+        screen.blit(b.image, b.position)
 
     display.flip() # esto coge todo lo que se ha hecho, y lo mete en la ventana
