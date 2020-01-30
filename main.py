9ab3641045f94b9b86a95bb69e939d27d0168672
Revellyon <martincondegrande@hotmail.com>
2020-01-30 04:02:07 +0100

    Utilizando el módulo pygame.sprite (Sprites y Groups)

--- main.py
+++ main.py
@@ -5,12 +5,19 @@ from random import randint
 
 init() #inicia pygame, si hubiesemos puesto import pygame, esto seria, pygame.init()
 FPS = 60
-class Bomb:
+
+class Bomb(sprite.Sprite):
     pictures = ['bomb_01.png', 'bomb_02.png','bomb_03.png','bomb_04.png']
+    w = 44
+    h = 42
 
     def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
+
+        sprite.Sprite.__init__(self) #constructor de quien heredamos para que asi tengamos todos los metodos y cuestiones
+
+        self.rect = Rect(self.x, self.y, self.w, self.h) #pygame.Rect
 
         self.frames = [] #con esto tenemos ya las imagenes, no las rutas a las imagenes
         for pict in self.pictures:
@@ -22,7 +29,7 @@ class Bomb:
         self.num_frames = len(self.frames)
 
         self.current_time = 0 #tiempo actual
-        self.animation_time = FPS//444 #fraccion de las FPS
+        self.animation_time = FPS + 150 #fraccion de las FPS
 
     def update(self, dt):
         self.current_time += dt
@@ -33,29 +40,32 @@ class Bomb:
                 self.frame_act = 0
             #ésto es lo mismo que de arriba ---> self.frame_act = (self.frame_act + 1) % self.num_frames
 
-    @property #ahora es un propiedad, no un método, como image
-    def position(self):
-        return self.x, self.y #esto lo devuelve en forma de tupla (x, y)
 
-    @property
+    @property #ahora es un propiedad, no un método, como image
     def image(self):
         return self.frames[self.frame_act]
 
 
 
-class Robot:
+class Robot(sprite.Sprite):
     speed = 5
     pictures = ['robot_r01.png', 'robot_r02.png', 'robot_r03.png', 'robot_r04.png']
+    w = 64 #altura robot, lo pone en la imagen
+    h = 68 #ancho robot
 
     def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
 
+        sprite.Sprite.__init__(self)
+
+        self.rect = Rect(self.x, self.y, self.w, self.h)
+
         self.frames = [] #con esto tenemos ya las imagenes, no las rutas a las imagenes
         for pict in self.pictures:
             frame = image.load('resources/{}'.format(pict)).convert_alpha()
             self.frames.append(frame)
-       #ésto es como lo de arriba ---> self.frames = [image.load('resources/{}'.format(pict)).convert_alpha() for pict in self.pictures]
+        #ésto es como lo de arriba ---> self.frames = [image.load('resources/{}'.format(pict)).convert_alpha() for pict in self.pictures]
 
         self.frame_act = 0 #disfraz activo, el 0 es posicion de quieto
         self.num_frames = len(self.frames) #numero de disfraz
@@ -71,47 +81,49 @@ class Robot:
         if self.y > 0: #si 'y' es mayor que 0
             self.y -= self.speed #restas para que suba
         '''
-        self.y = max(0, self.y - self.speed) #self.y es el maximo entre 0 y self.y - movimiento, si es 100, pues 95 ya que speed es 5, si es cero, pues 0 ,ya que entre 0 y -5 el max es 0
+        self.rect.y = max(0, self.rect.y - self.speed) #self.y es el maximo entre 0 y self.y - movimiento, si es 100, pues 95 ya que speed es 5, si es cero, pues 0 ,ya que entre 0 y -5 el max es 0
         self.change_frame()   
 
     def go_down(self):
-        self.y = min(600, self.y + self.speed)
+        self.rect.y = min(600, self.rect.y + self.speed)
         self.change_frame()
 
     def go_right(self):
-        self.x = min(800, self.x + self.speed)
+        self.rect.x = min(800, self.rect.x + self.speed)
         self.change_frame()
 
     def go_left(self):
-        self.x = max(0, self.x - self.speed)
+        self.rect.x = max(0, self.rect.x - self.speed)
         self.change_frame()
-
-    @property #ahora es un propiedad, no un método, como image
-    def position(self):
-        return self.x, self.y #esto lo devuelve en forma de tupla (x, y)
 
     @property
     def image(self):
         return self.frames[self.frame_act]
 
 
-
 class Game:
     clock = time.Clock() #pygame.time.clock
 
     def __init__(self):
-       self.screen = display.set_mode((800, 600)) #control de pantalla, parentesis devuelve la instancia de una ventana con el tamaño que demos, pygame.display...como arriba
-       display.set_caption('Hola mundo!') #titulo, pygame.display...etc, va sin self por ser de pygame
+        self.screen = display.set_mode((800, 600)) #control de pantalla, parentesis devuelve la instancia de una ventana con el tamaño que demos, pygame.display...como arriba
+        display.set_caption('Hola mundo!') #titulo, pygame.display...etc, va sin self por ser de pygame
        
-       self.background_color = (150, 150, 222) #color de fondo pantalla
+        self.background_color = (150, 150, 222) #color de fondo pantalla
+       
+        self.player_group = sprite.Group()
+        self.bombs_group = sprite.Group()
+        self.all_group = sprite.Group()
 
-       self.robot = Robot(400, 300)
+        self.robot = Robot(400, 300)
+        self.player_group.add(self.robot)
 
-       self.bombas = []
-       for i in range(5):
-          self.bomb = Bomb(randint(0 ,750), randint(0, 550))
-          self.bombas.append(self.bomb)
+       
+        for i in range(5):
+            bomb = Bomb(randint(0 ,750), randint(0, 550))
+            self.bombs_group.add(bomb)
 
+        self.all_group.add(self.robot, self.bombs_group)
+        
     def gameOver(self):
         quit()
         sys.exit()
@@ -150,11 +162,14 @@ class Game:
             
                 #...Procesar el resto de eventos
             self.screen.fill(self.background_color) #sobre screen, le metemos el color
+            '''
             self.screen.blit(self.robot.image, self.robot.position) #pones esa imagen donde te digo, ya no hace falta poner robot.position() al poner arriba @property
             for b in self.bombas:
                 b.update(dt)
                 self.screen.blit(b.image, b.position)
-
+            '''
+            self.all_group.update(dt)
+            self.all_group.draw(self.screen) #me lo pintas en la pantalla
             display.flip() # esto coge todo lo que se ha hecho, y lo mete en la ventana
 
 if __name__ == '__main__':
